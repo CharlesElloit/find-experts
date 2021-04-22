a43764ef3d05f158a18562f181b220fb00180449
Charles <aciforcharles@gmail.com>
2021-04-22 04:44:14 -0700

    Added an export for Rating model

--- src/models/index.js
+++ src/models/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   User: require("./User.model"),
   Rating: require("./Rating.model"),
+  Review: require("./Review.model"),
   Service: require("./Service.model"),
   Employer: require("./Employer.model"),
   Contractor: require("./Contractor.model"),

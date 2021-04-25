855bd9efbc58f9c77fbef30dad63dbe78d1b3d6e
Charles <aciforcharles@gmail.com>
2021-04-25 12:12:41 -0700

    added couple some error checks

--- middlewares/userAuthentication.js
+++ middlewares/userAuthentication.js
@@ -4,7 +4,7 @@ exports.auth = async (req, res, next) => {
   try {
     let token;
     if (req.headers.authorization && req.headers.authorization.startsWith("Bearer ")) {
-      const [x, y] = req.headers.authorization.split(" ");
+      const [_x, y] = req.headers.authorization.split(" ");
       token = y;
     } else {
       return res.status(401).json({
@@ -13,6 +13,7 @@ exports.auth = async (req, res, next) => {
     }
     // varify the token
     const verifiedToken = await jwt.verify(token, process.env.SECRET_KEY);
+
     if (!verifiedToken) {
       return res.status(403).json({
         error: "Sorry you are unauthorized to access this resources!",
@@ -22,8 +23,17 @@ exports.auth = async (req, res, next) => {
     // pass it on to the next middleware in the chain or stack.
     next();
   } catch (error) {
-    res.status(500).json({
-      authenticationError: "Your authentication fail please try again!",
-    });
+    if (error.message === "jwt expired") {
+      res.status(400).json({
+        success: false,
+        message: "Access token expires",
+      });
+    } else {
+      console.loe(error);
+      res.status(403).json({
+        error,
+        message: "Your authentication fail please try again!",
+      });
+    }
   }
 };

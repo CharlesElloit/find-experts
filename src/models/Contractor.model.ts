5f557a17c1320ca37b4e389fe80fb3e78f4ca4ba
Charles <aciforcharles@gmail.com>
2021-04-27 15:06:12 -0700

    rs/serviceController/index.ts

---
+++ src/models/Contractor.model.ts
@@ -0,0 +1,58 @@
+import Joi from "joi";
+import mongoose from "mongoose";
+
+// google how to validate an object using joi.
+export const contractorValidator = (data: object) => {
+  const schema = Joi.object({
+    occupation: Joi.string().required().trim().strip(),
+    cellphone: Joi.string().trim().required().strip(),
+    location: Joi.required(),
+    status: Joi.string().trim().required().strip(),
+    price: Joi.number().required(),
+    bio: Joi.string().max(200).trim().strip(),
+  });
+
+  return schema.validate(data);
+};
+
+const { Schema, model } = mongoose;
+
+const contractorSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    occupation: {
+      type: String,
+      trim: true,
+      max: 200,
+      required: [true, "occupation field is required"],
+    },
+    cellPhone: {
+      type: String,
+      required: [true, "cell phone field is required"],
+    },
+    location: {
+      type: {
+        type: String,
+        enum: ["Point"],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
+      address: { type: String, required: true },
+    },
+    status: { type: String, required: [true, "Status field is required"] },
+    price: { type: Number },
+    bio: { type: String, max: 600 },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export default model("Contractor", contractorSchema);

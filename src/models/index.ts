6780ec352ff5a61314f37b8c37fc5ae9559c2a27
Charles <aciforcharles@gmail.com>
2021-04-27 14:53:18 -0700

    converted entire project to use typescript

---
+++ src/models/index.ts
@@ -0,0 +1,15 @@
+import User from "./User.model";
+import Rating from "./Rating.model";
+import Review from "./Review.model";
+import Service from "./Service.model";
+import Employer from "./Employer.model";
+import Contractor from "./Contractor.model";
+
+export default {
+  User,
+  Rating,
+  Review,
+  Service,
+  Employer,
+  Contractor,
+};

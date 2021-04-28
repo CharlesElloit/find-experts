d1678793242a5244b43c43199734f661373431b8
Charles <aciforcharles@gmail.com>
2021-04-28 14:35:12 -0700

    added types support using typescript interface

--- src/models/Employer.model.ts
+++ src/models/Employer.model.ts
@@ -1,27 +1,34 @@
-const mongoose = require("mongoose");
+import mongoose, { Schema } from "mongoose";
+import IEmployer from "../interfaces/employer-interface";
 
-const { Schema, model } = mongoose;
-
-const employerSchema = new Schema({
-  userId: {
-    type: Schema.Types.ObjectId, ref: "User", required: true,
-  },
-  cellPhone: { type: String, required: [true, "cell phone field is required"] },
-  officeOrHomeNo: { type: String },
-  location: {
-    type: {
+const employerSchema: Schema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    cellPhone: {
       type: String,
-      enum: ["Point"],
-      required: true,
+      required: [true, "cell phone field is required"],
     },
-    coordinates: {
-      type: [Number],
-      required: true,
+    officeOrHomeNo: { type: String },
+    location: {
+      type: {
+        type: String,
+        enum: ["Point"],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
+      address: { type: String },
     },
-    address: { type: String },
   },
-}, {
-  timestamp: true,
-});
+  {
+    timestamps: true,
+  },
+);
 
-module.exports = model("Employer", employerSchema);
+export default mongoose.model<IEmployer>("Employer", employerSchema);

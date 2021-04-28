3b42ebb23005422bcf70c46c45efe045866b37d3
Charles <aciforcharles@gmail.com>
2021-04-28 14:36:28 -0700

    added types support using typescript interface

--- src/models/Rating.model.ts
+++ src/models/Rating.model.ts
@@ -1,29 +1,31 @@
-const mongoose = require("mongoose");
-const Joi = require("joi");
+import Joi from "joi";
+import mongoose, { Schema } from "mongoose";
+import IRating from "../interfaces/rating-interface";
 
-const { Schema, model } = mongoose;
-
-exports.ratingValidation = (data) => {
+exports.ratingValidation = (data: object): object => {
   const schema = Joi.object({
     rating: Joi.number().min(0).max(5),
   });
   return schema.validate(data);
 };
 
-const ratingSchema = new Schema({
+const ratingSchema: Schema = new Schema({
   userId: {
-    type: Schema.Types.ObjectId, ref: "User",
+    type: Schema.Types.ObjectId,
+    ref: "User",
   },
   rating: { type: Number, default: 0 },
   created: {
     At: { type: Date, default: Date.now },
     created_by: {
-      type: Schema.Types.ObjectId, ref: "User",
+      type: Schema.Types.ObjectId,
+      ref: "User",
     },
   },
   updated: {
-    type: Date, default: Date.now,
+    type: Date,
+    default: Date.now,
   },
 });
 
-module.exports = model("Rating", ratingSchema);
+export default mongoose.model<IRating>("Rating", ratingSchema);

d594cc341eb9156b4dc5f0706781f99cf53ac602
Charles <aciforcharles@gmail.com>
2021-04-28 14:38:22 -0700

    added types support for Service model (interface)

--- src/models/Service.model.ts
+++ src/models/Service.model.ts
@@ -1,40 +1,48 @@
 /* eslint-disable func-names */
-const mongoose = require("mongoose");
+import mongoose, { Schema } from "mongoose";
+import IService from "../interfaces/service-interface";
 
-const { Schema, model } = mongoose;
-
-const serviceSchema = new Schema({
-  name: {
-    type: String, trim: true, required: [true, "Service name field is required"],
-  },
-  serviceDate: { type: Date },
-  serviceTime: { type: Date },
-  selectContractor: {
-    type: Schema.Types.ObjectId, ref: "Contractor", required: true,
-  },
-  contractors: [{ type: Schema.Types.ObjectId, ref: "Contractor" }],
-  employerId: {
-    type: Schema.Types.ObjectId, ref: "Employer", required: true,
-  },
-  priceRange: { type: Number },
-  pending: { type: Boolean },
-  location: {
-    type: {
+const serviceSchema: Schema = new Schema(
+  {
+    name: {
       type: String,
-      enum: ["Point"],
+      trim: true,
+      required: [true, "Service name field is required"],
+    },
+    serviceDate: { type: Date },
+    serviceTime: { type: Date },
+    selectContractor: {
+      type: Schema.Types.ObjectId,
+      ref: "Contractor",
       required: true,
     },
-    coordinates: {
-      type: [Number],
+    contractors: [{ type: Schema.Types.ObjectId, ref: "Contractor" }],
+    employerId: {
+      type: Schema.Types.ObjectId,
+      ref: "Employer",
       required: true,
     },
-    address: { type: String, required: [true, "Address is field required"] },
+    priceRange: { type: Number },
+    pending: { type: Boolean },
+    location: {
+      type: {
+        type: String,
+        enum: ["Point"],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
+      address: { type: String, required: [true, "Address is field required"] },
+    },
   },
-}, {
-  timestamp: true,
-});
+  {
+    timestamps: true,
+  },
+);
 
-serviceSchema.pre("save", function (next) {
+serviceSchema.pre("save", function (next: Function) {
   const date = new Date();
   if (!this.serviceDate) {
     this.serviceDate = date;
@@ -46,4 +54,4 @@ serviceSchema.pre("save", function (next) {
   next();
 });
 
-module.exports = model("Service", serviceSchema);
+export default mongoose.model<IService>("Service", serviceSchema);

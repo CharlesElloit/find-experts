5ca599daca1c6bcff91f148f94ba7a7b03caffaf
Charles <aciforcharles@gmail.com>
2021-04-28 14:34:15 -0700

    Added typescript interface

--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -1,26 +1,41 @@
 /* eslint-disable func-names */
-const mongoose = require("mongoose");
-const jwt = require("jsonwebtoken");
+import jwt from "jsonwebtoken";
+import mongoose, { Schema } from "mongoose";
+import IUser from "../interfaces/user-interface";
 
-const { Schema, model } = mongoose;
-
-const userSchema = new Schema({
-  name: {
-    type: String, trim: true, min: 3, max: 100, required: [true, "Name field is required"],
+const userSchema: Schema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      min: 3,
+      max: 100,
+      required: [true, "Name field is required"],
+    },
+    email: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      max: 255,
+      required: [true, "Email field is required"],
+    },
+    isContractor: { type: Boolean, default: false },
+    password: {
+      type: String,
+      max: 255,
+      required: [true, "Password field must not be empty"],
+    },
+    resetPasswordToken: { type: String, max: 255 },
+    resetPasswordTokenExpires: { type: Date },
   },
-  email: {
-    type: String, unique: true, lowercase: true, trim: true, max: 255, required: [true, "Email field is required"],
+  {
+    timestamps: true,
   },
-  isContractor: { type: Boolean, default: false },
-  password: { type: String, max: 255, required: [true, "Password field must not be empty"] },
-  resetPasswordToken: { type: String, max: 255 },
-  resetPasswordTokenExpires: { type: Date },
-}, {
-  timestamps: true,
-});
+);
 
-userSchema.statics.generateAccessToken = function () {
-  const payload = {
+userSchema.statics.generateAccessToken = function (): object {
+  const payload: object = {
     userId: this._id,
     email: this.email,
     name: this.name,
@@ -37,4 +52,4 @@ userSchema.statics.generateRefreshToken = function () {
   return jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: "7d" });
 };
 
-module.exports = model("User", userSchema);
+export default mongoose.model<IUser>("User", userSchema);
